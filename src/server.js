94aa843e79cebd4cc4ecbab4709198e936fb063f
harithsenura <76419143+harithsenura@users.noreply.github.com>
2025-05-18 03:11:43 +0530

    Update server.js

--- src/server.js
+++ src/server.js
@@ -16,7 +16,7 @@ app.use(express.json())
 app.use(
   cors({
     credentials: true,
-    origin: ["http://localhost:3000"],
+    origin: ["https://itpm-backend-production.up.railway.app"],
   }),
 )
 
